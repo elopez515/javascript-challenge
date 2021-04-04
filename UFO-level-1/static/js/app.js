5172b45c7f6cf82d01453639fdefea8b1a897622
Efrain Lopez <e.lopez515@outlook.com>
2021-04-03 19:10:21 -0700

    completed the event handler function that will update our table data based on the change in our form

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -13,3 +13,65 @@ form.on("submit",UFO_Data);
 
 // Select the table body of our table in the html 
 var table_body = d3.select("tbody");
+
+// Complete the event handler function for the form
+function UFO_Data() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var user_date = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var user_input = user_date.property("value");
+
+    console.log(user_input);
+    console.log(tableData);
+
+    var filtered_data = tableData.filter(date => date.datetime === user_input);
+
+    console.log(filtered_data);
+
+    // Create an array for the city,state,country,shape,duration, & comments
+    // var city = filtered_data.map(date => date.city);
+    // var state = filtered_data.map(date => date.state);
+    // var country = filtered_data.map(date => date.country);
+    // var shape = filtered_data.map(date => date.shape);
+    // var durationMinutes = filtered_data.map(date => date.durationMinutes);
+    // var comments = filtered_data.map(date => date.comments);
+
+    // Select the table body of our table in the html 
+    // var table_body = d3.select("tbody");
+
+    table_body.html("");
+
+    // var tr = = 
+
+    // table_body.append("td").text(`${city}`)
+    // table_body.append("td").text(`${city}${state}${country}${shape}${durationMinutes}${comments}`)
+
+//     filtered_data.forEach(ufo_info => {
+//         let row = document.createElement("tr");
+
+//         Object.values(ufo_info).forEach(info =>{
+//             let cell = document.createElement("td");
+//             cell.appendChild(document.createTextNode(info));
+//             // cell.text(info)
+//             row.appendChild(cell);
+//         })
+//         table_body.appendChild(row);
+//     })
+//     // table_body.appendChild(table).text(info);
+
+
+    filtered_data.forEach(ufo_info => {
+        let row = table_body.append("tr");
+
+        Object.values(ufo_info).forEach(info =>{
+            let cell = row.append("td").text(info);
+        })
+    })
+    // table_body.appendChild(table).text(info);
+
+};
