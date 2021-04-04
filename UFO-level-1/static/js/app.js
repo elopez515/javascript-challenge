a1ea518dc15fcaee1de638e0edf829034d2b465c
Efrain Lopez <e.lopez515@outlook.com>
2021-04-03 19:22:25 -0700

    cleaned up script and added code comments

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -29,42 +29,13 @@ function UFO_Data() {
     console.log(user_input);
     console.log(tableData);
 
+    // Create a variable for our table data that will filter based on date
     var filtered_data = tableData.filter(date => date.datetime === user_input);
 
     console.log(filtered_data);
 
-    // Create an array for the city,state,country,shape,duration, & comments
-    // var city = filtered_data.map(date => date.city);
-    // var state = filtered_data.map(date => date.state);
-    // var country = filtered_data.map(date => date.country);
-    // var shape = filtered_data.map(date => date.shape);
-    // var durationMinutes = filtered_data.map(date => date.durationMinutes);
-    // var comments = filtered_data.map(date => date.comments);
-
-    // Select the table body of our table in the html 
-    // var table_body = d3.select("tbody");
-
-    table_body.html("");
-
-    // var tr = = 
-
-    // table_body.append("td").text(`${city}`)
-    // table_body.append("td").text(`${city}${state}${country}${shape}${durationMinutes}${comments}`)
-
-//     filtered_data.forEach(ufo_info => {
-//         let row = document.createElement("tr");
-
-//         Object.values(ufo_info).forEach(info =>{
-//             let cell = document.createElement("td");
-//             cell.appendChild(document.createTextNode(info));
-//             // cell.text(info)
-//             row.appendChild(cell);
-//         })
-//         table_body.appendChild(row);
-//     })
-//     // table_body.appendChild(table).text(info);
-
-
+    // Create a forEach function that will loop through
+    // and append our filtered data based on date inputted by user
     filtered_data.forEach(ufo_info => {
         let row = table_body.append("tr");
 
@@ -72,6 +43,5 @@ function UFO_Data() {
             let cell = row.append("td").text(info);
         })
     })
-    // table_body.appendChild(table).text(info);
 
 };
