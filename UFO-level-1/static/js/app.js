b6b3ea4f3bb1643cea81f33aced5f0423372db4e
Efrain Lopez <e.lopez515@outlook.com>
2021-04-03 19:04:49 -0700

    added lines to select our tags form the html file and created event handlers

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select the button tag from out index.html
+var button = d3.select("#filter-btn");
+
+// Select the form tag from out index.html
+var form = d3.select("form");
+
+// Create event handlers 
+button.on("click", UFO_Data);
+form.on("submit",UFO_Data);
+
+// Select the table body of our table in the html 
+var table_body = d3.select("tbody");
